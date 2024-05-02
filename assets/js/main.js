8302cf52ff59bec359de5a151abf51111e2821df
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-05-02 11:34:46 +0200

    voilà

---
+++ assets/js/main.js
@@ -0,0 +1,96 @@
+//INTRO TYPE WRITER
+
+const title=document.querySelector('h1');
+const titleText=title.innerText;
+const titleArray=titleText.split('');
+let titleProgress='';
+
+title.innerText='';
+
+async function typeWriter() {
+    for(let i=0; i<titleArray.length; i++) {
+        const delay=Math.floor(Math.random() * 300)+200;
+        await sleep(delay);
+        titleProgress+=titleArray[i];
+        title.innerText=titleProgress;
+    }
+}
+typeWriter();
+
+//ELAPSED TIME
+const timeDisplay=document.querySelector('#elapsed');
+let minute,timer=0;
+
+async function elapsedTime() {
+    while(true) {
+        await sleep(1000);
+        timer+=1;
+        if(timer<60) {
+            timeDisplay.innerText='Just arrived...';
+        } else {
+            minute=Math.floor(timer/60);
+            if(minute==1) {
+                timeDisplay.innerText='a minute has passed.';
+            } else {
+                timeDisplay.innerText=minute+' minutes have passed.';
+            }
+        }
+    }
+}
+elapsedTime();
+
+
+//CREATE TABLE
+const table=document.querySelector('#table');
+
+for(let i=0; i<12; i++) {
+    const div=createDiv('div',table,null,'hole');
+    div.addEventListener('click', () => {
+        hammer(div);
+    })
+}
+
+//START GAME
+const button=document.querySelector('#startButton');
+const score=document.querySelector('#scoreDisplay');
+
+button.addEventListener('click', () => {
+    button.value='Restart';
+    score.innerText='0';
+    button.style.display='none';
+    randomMole();
+})
+
+const holes=Array.from(document.querySelector('#table').children);
+const creationDelay=3000;
+const visibleDelay=1000;
+const intervalDelay=500;
+
+async function randomMole() {
+    while(true) {
+        let delay=Math.floor(Math.random() * creationDelay);
+        await sleep (delay);
+
+        const randomHole=Math.floor(Math.random() * holes.length);
+        holes[randomHole].classList.add('mole');
+        
+        const randomVisibility=Math.floor(Math.random() * (visibleDelay+300));
+        await sleep(randomVisibility);
+        holes[randomHole].classList.remove('mole');
+
+        await sleep(Math.floor(Math.random() * intervalDelay));
+    }
+}
+
+//PLAY
+function hammer(div) {
+    const classes=div.classList;
+    if(classes.contains('mole')) {
+        score.innerText=parseInt(score.innerText)+1;
+        div.classList.remove('mole');
+    } else {
+        if(parseInt(score.innerText) > 0) {
+          score.innerText=parseInt(score.innerText)-1;  
+        }
+    }
+}
