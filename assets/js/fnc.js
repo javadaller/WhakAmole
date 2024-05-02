86a2815a52c15b1ef8fd6203341b255b441567b2
Arnaud Van Acker <arnaudvanacker@yahoo.fr>
2024-05-02 11:51:47 +0200

    bug

--- assets/js/fnc.js
+++ assets/js/fnc.js
@@ -1,7 +1,7 @@
 // CREATE DIV
 function createDiv(type,parent,content,className) {
   const newDiv=document.createElement(type);
-  if (type!='') {
+  if (type!=null) {
     newDiv.innerHTML=content;
   }
   if (className!=null) {
@@ -15,34 +15,4 @@ function createDiv(type,parent,content,className) {
 // SLEEP FUNCTION
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-//------------------------------------------------------------------------------------------
-
-// ESCAPE HTML
-function escapeHtml(text) {
-  var map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#039;'
-  };
-  
-  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-}
-
-//------------------------------------------------------------------------------------------
-
-// IS MOBILE
-function isMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-
-//------------------------------------------------------------------------------------------
-
-// RANDOM COLOR
-function randomColor() {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  return '#'+randomColor;
 }
